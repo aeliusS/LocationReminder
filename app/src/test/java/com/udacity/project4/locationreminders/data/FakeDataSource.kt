4c246f5667d41ab432ebf171edd867c3919e7a1d
aeliusS <58757834+aeliusS@users.noreply.github.com>
2022-08-31 17:30:08 -0400

    working on testing RemindersListViewModel

--- app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/data/FakeDataSource.kt
@@ -1,28 +1,51 @@
 package com.udacity.project4.locationreminders.data
 
+import androidx.lifecycle.MutableLiveData
 import com.udacity.project4.locationreminders.data.dto.ReminderDTO
 import com.udacity.project4.locationreminders.data.dto.Result
+import com.udacity.project4.locationreminders.data.dto.Result.*
 
 //Use FakeDataSource that acts as a test double to the LocalDataSource
 class FakeDataSource : ReminderDataSource {
 
-//    TODO: Create a fake data source to act as a double to the real data source
+    var remindersServiceData: LinkedHashMap<String, ReminderDTO> = LinkedHashMap()
+
+    // private val reminders = MutableLiveData<List<ReminderDTO>>()
+
+    private var shouldReturnError = false
+
+    fun setReturnError(value: Boolean) {
+        shouldReturnError = value
+    }
 
     override suspend fun getReminders(): Result<List<ReminderDTO>> {
-        TODO("Return the reminders")
+        if (shouldReturnError) {
+            return Error("Test exception")
+        }
+        return Success(remindersServiceData.values.toList())
     }
 
     override suspend fun saveReminder(reminder: ReminderDTO) {
-        TODO("save the reminder")
+        remindersServiceData[reminder.id] = reminder
     }
 
     override suspend fun getReminder(id: String): Result<ReminderDTO> {
-        TODO("return the reminder with the id")
+        if (shouldReturnError) {
+            return Error("Test exception")
+        }
+        remindersServiceData[id]?.let {
+            return Success(it)
+        }
+        return Error("Could not find task")
     }
 
     override suspend fun deleteAllReminders() {
-        TODO("delete all the reminders")
+        remindersServiceData.clear()
     }
 
-
+    fun addReminders(vararg reminders: ReminderDTO) {
+        for (reminder in reminders) {
+            remindersServiceData[reminder.id] = reminder
+        }
+    }
 }
--- app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
+++ app/src/test/java/com/udacity/project4/locationreminders/reminderslist/RemindersListViewModelTest.kt
@@ -1,13 +1,72 @@
 package com.udacity.project4.locationreminders.reminderslist
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.udacity.project4.locationreminders.data.FakeDataSource
+import com.udacity.project4.locationreminders.data.ReminderDataSource
+import com.udacity.project4.locationreminders.data.dto.ReminderDTO
+import com.udacity.project4.locationreminders.getOrAwaitValue
 import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.koin.core.context.loadKoinModules
+import org.koin.core.context.startKoin
+import org.koin.dsl.module
+import org.koin.test.KoinTest
+import org.koin.test.get
+import kotlin.test.assertNotNull
+import org.hamcrest.CoreMatchers.*
+import org.hamcrest.MatcherAssert.assertThat
 
 @RunWith(AndroidJUnit4::class)
 @ExperimentalCoroutinesApi
-class RemindersListViewModelTest {
+class RemindersListViewModelTest : KoinTest {
 
     //TODO: provide testing to the RemindersListViewModel and its live data objects
+
+    // subject under test
+    private lateinit var remindersListViewModel: RemindersListViewModel
+
+    // use a fake repository to be injected into the ViewModel
+    private lateinit var dataSource: FakeDataSource
+
+    @Before
+    fun setupViewModel() {
+        // initialize the datasource with 3 reminders
+        dataSource = FakeDataSource()
+        val reminder1 = ReminderDTO("Title1", "Description1", "Sydney", 1.0, 2.0)
+        val reminder2 = ReminderDTO("Title2", "Description2", "Sydney", 1.0, 2.0)
+        val reminder3 = ReminderDTO("Title3", "Description3", "Sydney", 1.0, 2.0)
+        dataSource.addReminders(reminder1, reminder2, reminder3)
+
+        val module = module {
+            single { RemindersListViewModel(get(), dataSource) }
+        }
+        loadKoinModules(module)
+        remindersListViewModel = get()
+    }
+
+    // this makes the test run sequentially, which is important for testing LiveData variables
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    // TODO: rename function
+    @Test
+    fun testingComponent() {
+//        startKoin {
+//            modules(
+//                module {
+//                    single { RemindersListViewModel(get(), dataSource) }
+//                }
+//            )
+//        }
+        assertNotNull(remindersListViewModel)
+        // TODO: call load function before check
+        val value = remindersListViewModel.remindersList.getOrAwaitValue()
+        assertThat(value, (not(nullValue())))
+    }
 
 }
