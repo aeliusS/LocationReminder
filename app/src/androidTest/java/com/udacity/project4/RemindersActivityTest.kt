35d629d82576bc0c9d0a70e145f21467415329a7
aeliusS <58757834+aeliusS@users.noreply.github.com>
2022-09-11 23:37:02 -0400

    working on end to end test

--- app/src/androidTest/java/com/udacity/project4/RemindersActivityTest.kt
+++ app/src/androidTest/java/com/udacity/project4/RemindersActivityTest.kt
@@ -1,16 +1,34 @@
 package com.udacity.project4
 
 import android.app.Application
+import androidx.test.core.app.ActivityScenario
 import androidx.test.core.app.ApplicationProvider.getApplicationContext
+import androidx.test.espresso.Espresso.onIdle
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import androidx.test.espresso.action.ViewActions.*
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.*
+import com.firebase.ui.auth.AuthUI
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.udacity.project4.locationreminders.RemindersActivity
 import com.udacity.project4.locationreminders.data.ReminderDataSource
 import com.udacity.project4.locationreminders.data.local.LocalDB
 import com.udacity.project4.locationreminders.data.local.RemindersLocalRepository
 import com.udacity.project4.locationreminders.reminderslist.RemindersListViewModel
 import com.udacity.project4.locationreminders.savereminder.SaveReminderViewModel
+import com.udacity.project4.util.DataBindingIdlingResource
+import com.udacity.project4.util.monitorActivity
+import com.udacity.project4.utils.EspressoIdlingResource
 import kotlinx.coroutines.runBlocking
+import org.hamcrest.CoreMatchers.`is`
+import org.junit.After
 import org.junit.Before
+import org.junit.Test
 import org.junit.runner.RunWith
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.context.startKoin
@@ -18,6 +36,7 @@ import org.koin.core.context.stopKoin
 import org.koin.dsl.module
 import org.koin.test.AutoCloseKoinTest
 import org.koin.test.get
+import kotlin.test.assertFailsWith
 
 @RunWith(AndroidJUnit4::class)
 @LargeTest
@@ -27,6 +46,9 @@ class RemindersActivityTest :
 
     private lateinit var repository: ReminderDataSource
     private lateinit var appContext: Application
+    private lateinit var auth: FirebaseAuth
+    private val dataBindingIdlingResource = DataBindingIdlingResource()
+    private val idlingResource = EspressoIdlingResource.countingIdlingResource
 
     /**
      * As we use Koin as a Service Locator Library to develop our code, we'll also use Koin to test our code.
@@ -49,7 +71,7 @@ class RemindersActivityTest :
                     get() as ReminderDataSource
                 )
             }
-            single { RemindersLocalRepository(get()) }
+            single<ReminderDataSource> { RemindersLocalRepository(get()) }
             single { LocalDB.createRemindersDao(appContext) }
         }
         //declare a new koin module
@@ -63,9 +85,67 @@ class RemindersActivityTest :
         runBlocking {
             repository.deleteAllReminders()
         }
+        auth = FirebaseAuth.getInstance()
+    }
+
+    @Before
+    fun registerIdlingResource() {
+        IdlingRegistry.getInstance().register(idlingResource)
+        IdlingRegistry.getInstance().register(dataBindingIdlingResource)
+    }
+
+    @After
+    fun unregisterIdlingResource() {
+        IdlingRegistry.getInstance().unregister(idlingResource)
+        IdlingRegistry.getInstance().unregister(dataBindingIdlingResource)
     }
 
 
-//    TODO: add End to End testing to the app
+    // TODO: add End to End testing to the app
+
+    @Test
+    fun test_logout() {
+        if (auth.currentUser == null) {
+            login()
+            idlingResource.increment()
+        }
+        // start up the reminder screen
+        val activityScenario = ActivityScenario.launch(RemindersActivity::class.java)
+        dataBindingIdlingResource.monitorActivity(activityScenario)
+
+        // if logged in, log out
+        if (auth.currentUser != null) {
+            onView(withText("LOGOUT")).check(matches(isDisplayed()))
+            onView(withText("LOGOUT")).perform(click())
+        }
+
+        // verify that the logout screen is displayed
+        onView(withText("LOGIN")).check(matches(isDisplayed()))
+
+        // close out the activity
+        activityScenario.close()
+
+    }
+
+    private fun login() {
+        auth
+            .signInWithEmailAndPassword("testing@example.com", "asdf1234")
+            .addOnCompleteListener() { task ->
+                assertThat(task.isSuccessful, `is`(true))
+                idlingResource.decrement()
+            }
+    }
+
+    private fun logout() {
+        AuthUI.getInstance().signOut(appContext)
+    }
+
+
+    // TODO: add new reminder
+    @Test
+    fun addNewReminder() = runBlocking {
+        // start up reminder screen
+
+    }
 
 }
